d341131be0a14ffc0912c20f31b29520502210e0
Shin-sibainu <shincode0712@gmail.com>
2022-03-16 13:35:38 +0900

    done follow user

--- routes/users.js
+++ routes/users.js
@@ -65,14 +65,15 @@ router.put("/:id/follow", async (req, res) => {
       if (!user.followers.includes(req.body.userId)) {
         await user.updateOne({ $push: { followers: req.body.userId } });
         await currentUser.updateOne({ $push: { followings: req.params.id } });
+        res.status(200).json("user has been followd");
       } else {
-        res.status(403).json("user has been followed");
+        return res.status(403).json("you allready follow this user");
       }
     } catch (err) {
-      res.status(500).json(err);
+      return res.status(500).json(err);
     }
   } else {
-    res.status(500).json("cant follow yourself");
+    return res.status(500).json("cant follow yourself");
   }
 });
 
